c9c3f946dda648c5319a5dcf5b4bc827f9a94d5a
JuanPalbo <juan.cerizola@correo.ucu.edu.uy>
2023-11-08 11:39:09 -0300

    Coso

---
+++ src/Library/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Library;
+
+public sealed class Singleton<T> where T : new()
+{
+    public Singleton()
+    {
+    }
+
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new T();
+            }
+
+            return _instance;
+        }
+    }
+}
